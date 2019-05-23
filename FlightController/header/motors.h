1ff53d5fd6eb76d828db5eaf0ac78a165842dd42
Alexander Dada <alexdada555@gmail.com>
2019-05-24 00:37:01 +0100

    Add files via upload

--- FlightController/header/motors.h
+++ FlightController/header/motors.h
@@ -15,7 +15,7 @@
 class Motors                  // create motor control class
 {
   public:
-    double AltitudeControl(double input,double sensorVal);       // altitude hold algorithm
+    double AltitudeControl(double input,double sensorVal,double initial);       // altitude hold algorithm
     void RunMotors(Servo* Motor,int Gain);   // transmits commands to motors
     void MotorMix(Servo x, int y, int lower, int upper);  // motor mixing algorithm for roll, pitch and yaw motion
     void FlightControl(double v,double x,double y,double z);   // transmitts mixed values to motors
